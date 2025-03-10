e5626b2f0a5d1926031609ec51864288d1a4178a
VargaDot <vancouver_cliparf@simplelogin.com>
2025-03-10 21:04:02 +0100

    removed min health

--- vitality_data/vitality_data.gd
+++ vitality_data/vitality_data.gd
@@ -2,7 +2,6 @@ class_name VitalityData extends Resource
 
 @export_category("Vitality Stats")
 @export var max_health:int = 100
-@export var min_health:int
 @export var invincibility_time:float
 
 signal health_changed(health:int)
@@ -12,12 +11,11 @@ signal died()
 signal invincibility_started()
 signal invincibility_stopped()
 signal max_health_changed(max_health:int)
-signal min_health_changed(min_health:int)
 
 var current_health:int :
 	set(val):
 		var old_health = current_health
-		current_health = clamp(val, min_health, max_health)
+		current_health = clamp(val, 0, max_health)
 
 		match sign(current_health - old_health):
 			1:
@@ -30,7 +28,7 @@ var current_health:int :
 				print("Something went wrong in calculating health.")
 
 		health_changed.emit(current_health)
-		if current_health <= min_health:
+		if current_health <= 0:
 			died.emit()
 		emit_changed()
 
@@ -50,17 +48,13 @@ var is_hurt:bool = false :
 		emit_changed()
 
 func _init() -> void:
-	if max_health == min_health:
-		print("WARNING: MAXIMUM HEALTH IS EQUAL TO MINIMUM HEALTH")
+	if max_health == 0:
+		print("WARNING: MAXIMUM HEALTH IS 0")
 	current_health = max_health
 
 func set_max_health(new_value:int) -> void:
 	max_health = new_value
 	max_health_changed.emit(max_health)
-
-func set_min_health(new_value:int) -> void:
-	min_health = new_value
-	min_health_changed.emit(min_health)
 
 func set_invincibility_time(new_value:float) -> void:
 	invincibility_time = new_value
